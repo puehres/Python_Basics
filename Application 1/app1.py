97d1c1ae30416b859afc355ddacb297ddb309333
Puhringer <puhringer.fabian@kpmg.nl>
2019-06-28 00:12:30 +0200

    Finished first application

--- Application 1/app1.py
+++ Application 1/app1.py
@@ -1,9 +1,31 @@
 import json
+from difflib import get_close_matches
+
+with open(r"C:\Users\fpuhringer\github\Python_Course\Application 1\data.json") as json_file:
+    dict = json.load(json_file)
 
 def get_def(word):
-    with open(r"C:\Users\fpuhringer\github\Python_Course\Application 1\data.json") as json_file:
-        dict = json.load(json_file)
-        print(dict.get(word))
+    word = word.lower()
+    get_close_matches(word,dict.keys())[0]
+    if word in dict:
+        return(dict.get(word))
+    elif len(get_close_matches(word,dict.keys())) >  0:
+        choice = input("Did you mean %s instead? Enter Y if yes, or N if no:\n" % get_close_matches(word,dict.keys())[0])
+        if choice == "Y":
+            return dict[get_close_matches(word,dict.keys())[0]]
+        elif choice == 'N':
+            return "The word doesn't exit. Please double check it."
+        else:
+            return("We didn't understand your query.")
+    else:
+        return "The word doesn't exist. Please double check it."
 
 word = input("Please enter the word you are looking for:\n")
-get_def(word)
+
+output = get_def(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
